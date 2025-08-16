cf0530cf531fe29968f07c5628dcaec08b507cd1
abdulfattoh-dev <abdulfattoh.dev@gmail.com>
2025-08-16 11:33:35 +0500

    changed url

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,5 +5,5 @@ export const api = axios.create({
 })
 
 export const api2 = axios.create({
-    baseURL: 'https://interchange.proxy.rlwy.net:42407'
+    baseURL: 'car-production-9848.up.railway.app'
 })
